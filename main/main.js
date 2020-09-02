623b3d9c9d6690b9d4e3da6d815eb6fd375fa739
sanya6456 <66361881+sanya6456@users.noreply.github.com>
2020-09-02 14:47:01 +0200

    Finished!
    made by someones UI design! (full responsive)

--- main/main.js
+++ main/main.js
@@ -11,10 +11,32 @@ $(document).ready(function(){
         $('.responsive-menu').slideUp(500);
     });
 
-    //button active class
+    //button active class in title section
     $('.title-btn').click(function(){
         $(this).addClass('active-button');
         $(this).siblings().removeClass('active-button');
     })
 
+    //button active class in card section
+    $('.card-btn').click(function(){
+        $(this).addClass('active-button');
+        $(this).siblings().removeClass('active-button');
+    })
+
+
+    //swiper initialize X < 794px  !!!
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+    });
+
+
+
 });
+
